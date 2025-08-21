5b4588933cc51b6509aa34188bc6024e7b401094
Zareth Alejandra Rodriguez Padilla <zareth.alejandra@gmail.com>
2025-08-21 21:08:11 +0200

    Update Variables_1.java

--- ClaseCeroAle/src/Tipos_variables/Variables_1.java
+++ ClaseCeroAle/src/Tipos_variables/Variables_1.java
@@ -1,12 +1,11 @@
-package Tipos_variables;
+package clase1;
 
 import java.util.Scanner;
 
-public class Variables_1 {
+public class Variables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		int result, numero, edad = 16, caso = 12; //variables enteras
 		char sexo = 'f'; //datos char se guardan con una sola comilla
 		boolean flag = false; //variables logicas, solo dos, verdadero o falso
@@ -17,30 +16,36 @@ public class Variables_1 {
 		System.out.println("Hola mi nombre es "+ nombre + " y tengo "+ edad + " años, y comprè a Memo por: " + precio);
 		
 		Scanner captura = new Scanner(System.in);
-		
-		System.out.println("Selecciona una opciòn");
-		System.out.println("Suma 1");
-		System.out.println("Resta 2");
-		int opp = captura.nextInt();
-		
-		
-		switch(opp){
-			case 1: 
-				System.out.println("Valor 1");
-				int num1 = captura.nextInt();
-				System.out.println("Valor 2");
-				int num2 = captura.nextInt();
-				result = num1+num2;
-				System.out.println("Resultado "+ result);
-			break;
-			case 2:
-				System.out.println("Valor 1");
-				num1 = captura.nextInt();
-				System.out.println("Valor 2");
-				num2 = captura.nextInt();
-				result = num1-num2;
-				System.out.println("Resultado "+ result);
-			break;
-		}	
+		boolean ext = true;
+		while (ext == true) {
+			System.out.println("Selecciona una opciòn");
+			System.out.println("Suma 1");
+			System.out.println("Resta 2");
+			System.out.println("Salir 3");
+			int opp = captura.nextInt();
+			
+			switch(opp){
+				case 1: 
+					System.out.println("Valor 1");
+					int num1 = captura.nextInt();
+					System.out.println("Valor 2");
+					int num2 = captura.nextInt();
+					result = num1+num2;
+					System.out.println("Resultado "+ result);
+				break;
+				case 2:
+					System.out.println("Valor 1");
+					num1 = captura.nextInt();
+					System.out.println("Valor 2");
+					num2 = captura.nextInt();
+					result = num1-num2;
+					System.out.println("Resultado "+ result);
+				break;
+				case 3:
+					ext = false;
+				break;
+			
+			}	
+		}
 	}
 }
