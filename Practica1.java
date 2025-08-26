8f4cd4b26d0aa280209ccfec7c638766ccffd5c0
Zareth Alejandra Rodriguez Padilla <zareth.alejandra@gmail.com>
2025-08-26 20:45:38 +0200

    Add files via upload

--- Practica1.java
+++ Practica1.java
@@ -7,27 +7,48 @@ public class Practica1 {
 	public static void main(String[] args) {
 	
 	int[] numeros = new int[15];
-	int x;
+	int x, contador_neg = 0, promedio = 0, fin = 0, y =0;
 	
 	Scanner captura = new Scanner(System.in);
 	
-	for(x = 0; x < numeros.length; x++) {
-		int num, contador_neg = 0;
+	System.out.println("Escribe 14 valores");
+	for(x = 0; x < 16; x++) {
+		int num;
 		System.out.println("Dame el valor: "+ x);
 		num = captura.nextInt();
 		numeros[x] = num;
 		}
-	
+	System.out.println("Estos son los resultados:");
 	for(int i = 0; i < numeros.length; i ++) {
 		//String n = numeros[i] %2 == 0 ? "hola" : "no"; (if "cortito" segun Soraya)
-		if (numeros[i] %2 == 0 && numeros[i] > 0){ 
+		if (numeros[i] %2 == 0) {
 			double raiz;
 			raiz = Math.sqrt(numeros[i]);
-			System.out.println("La raiz cuadrada de "+ numeros[i]+" "+raiz);
+			if (raiz == (int) raiz && numeros[i]>0) {
+				System.out.println("La raiz cuadrada de "+ numeros[i]+" es  "+(int)raiz);
 			}
-		if (numeros[i] < 0){
-			contador_neg += 1;
 		}
+	}
+		System.out.println(" ");
+		for(int i = 0; i<numeros.length; i++) {
+			if (numeros[i] < 0) {
+				contador_neg++;
+			}
+		}
+		System.out.println("Total de numeros negativos"+ contador_neg);
+		for(int i=0; i>numeros.length; i++) {
+			if (numeros[i] != 0) {
+				promedio += numeros[i];
+				y++;
+			}
+		}
+		if (fin == 0) {
+			System.out.println("No hay numeros impares");
+		}
+		else {
+			fin = promedio/y;
+			System.out.println(" ");
+			System.out.println("Promedio de numeros impares: "+ fin);
 		}
 	}
 }
